69c6d1cf0a324886ae67863801470075b3cefb2f
Yu0823 <55206903+Yu0823@users.noreply.github.com>
2021-12-09 16:23:08 +0800

    improve

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#
-Based on Singo https://github.com/Gourouting/singo
+#Smart-Takeaway-Boxes-System
+Based on Go 1.16.5 and MySQL 8.0.27
--- server/router.go
+++ server/router.go
@@ -11,7 +11,7 @@ import (
 func NewRouter() *gin.Engine {
 	r := gin.Default()
 
-	// 中间件, 顺序不能改
+	// 中间件
 	//r.Use(middleware.Session(os.Getenv("SESSION_SECRET")))
 	r.Use(middleware.Cors())
 	r.Use(middleware.CurrentUser())
@@ -19,6 +19,7 @@ func NewRouter() *gin.Engine {
 	// 路由
 	v1 := r.Group("/api/v1")
 	{
+		//测试接口
 		v1.POST("ping", api.Ping)
 
 		//获取新的外卖柜
@@ -33,13 +34,13 @@ func NewRouter() *gin.Engine {
 		// 用户登录
 		v1.POST("user/login", api.UserLogin)
 
-		// 需要登录保护的
+		// 登录保护
 		auth := v1.Group("")
 		auth.Use(middleware.AuthRequired())
 		{
 			// User Routing
 			auth.GET("user/me", api.UserMe)
-			auth.DELETE("user/logout", api.UserLogout)
+			auth.POST("user/logout", api.UserLogout)
 		}
 	}
 	return r
